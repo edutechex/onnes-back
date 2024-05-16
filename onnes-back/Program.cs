877ad6f1afe6f4b08edccdbb029e7e6a18e0264a
Yangyaseni Nayak <msyangyaseni@gmail.com>
2024-05-16 14:39:15 +0530

    up to date

--- onnes-back/Program.cs
+++ onnes-back/Program.cs
@@ -1,7 +1,20 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Onnes.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("Onnes"));
+});
+ 
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));                                                                                                                                                                                                                    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,13 +22,29 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+app.UseDeveloperExceptionPage();
+app.UseRouting();
+app.UseStaticFiles();
+    
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+                   Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "images")),
+    RequestPath = new PathString("/images")
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "images")),
+    RequestPath = new PathString("/images")
+});
+
+app.UseCors("corspolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
